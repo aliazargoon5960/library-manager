4bf43d593b7099986945b15eb1d0e326b5def43c
AliAsghar Azargoon <aliazargoon5960@gmail.com>
2025-12-18 20:44:11 +0330

    edit main

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 import sys
 from PyQt5.QtWidgets import QApplication
-from PyQt5.QtCore import Qt 
 from Book.main_view import LibraryMainView
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    
-
-    app.setLayoutDirection(Qt.RightToLeft) 
-    
     window = LibraryMainView()
     window.show()
     sys.exit(app.exec_())
